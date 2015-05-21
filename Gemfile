d6f2bca915ce6ce8d2161575b9ee30d16b542cfb
jyranzo <javieryranzo@gmail.com>
2015-05-21 19:13:05 -0300

    bugs fix

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bootstrap-sass'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
+gem 'paperclip', '~> 4.2'
 
 group :development, :test do
      gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505015451) do
+ActiveRecord::Schema.define(version: 20150521141229) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "name"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
